685e2d9f97d84d0192d5c8351f9754a8af89b2bb
Max <48467432+Max-Frisch@users.noreply.github.com>
2023-02-03 14:28:14 +0700

    Create better_calculator.py
    refactored the calculator to use regular expressions, so any size of numbers can be used

---
+++ better_calculator.py
@@ -0,0 +1,39 @@
+import re
+
+def calculator(num1, operator, num2):
+    match operator:
+        case "+": return num1 + num2
+    match operator:
+        case "-": return num1 - num2
+    match operator:
+        case "*": return num1 * num2
+    match operator:
+        # using pythons ternery operator in the return statement ( a if <cond> else b)
+        case "/": return num1 / num2 if num2 != 0 else "Error, divided by 0!"
+
+print("")
+print("Better Calculator. Can use plus(+), minus(-), divide(/) or multiply(*) :-] ")
+print("")
+
+# saving the typed in calculation into a string, stripping white space
+raw_input_string = input("Write a calculation in the form of \"2+2\" -> ".strip())
+
+# extracting the operator and saving it in the operator variable
+if "+" in raw_input_string:
+    operator = "+"
+if "-" in raw_input_string:
+    operator = "-"
+if "*" in raw_input_string:
+    operator = "*"
+if "/" in raw_input_string:
+    operator = "/"
+# using regular expression to split the two operands into a list [num1, num2]
+input_string_list = re.split('\+|\-|\*|\/', raw_input_string)
+
+#casting the two (string) numbers into integers
+intnum1 = int(input_string_list[0])
+intnum2 = int(input_string_list[1])
+
+result = calculator(intnum1,operator,intnum2)
+print("")
+print(f"{intnum1} {operator} {intnum2} = {result}")
